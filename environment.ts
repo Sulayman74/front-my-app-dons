8c72cf560c59fd4135d1fc22075fab7bd1035ac4
MacMini de Sulayman <keohavong.sirikone@gmail.com>
2024-05-23 10:46:29 +0200

    connexion backend infomaniak

--- environment.ts
+++ environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseApiUrl: process.env["API_URL"]
+    baseApiUrl: "https://compte-dons.kappsi.tech/"
     // baseApiUrl: 'https://compte-dons-sulayman74s-projects.vercel.app/api/v1/'
 };
