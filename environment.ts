6daf979baf9edfd865b9110e67bfcf88566d071b
MacMini de Sulayman <keohavong.sirikone@gmail.com>
2024-05-23 10:58:31 +0200

    environment changed

--- environment.ts
+++ environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    baseApiUrl: "https://compte-dons.kappsi.tech/"
+    baseApiUrl: "https://compte-dons.kappsi.tech/api/v1/"
     // baseApiUrl: 'https://compte-dons-sulayman74s-projects.vercel.app/api/v1/'
 };
