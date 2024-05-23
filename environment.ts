5146abbdf6efc98ba8a66cbae61700dc344f10bd
MacMini de Sulayman <keohavong.sirikone@gmail.com>
2024-05-23 08:58:23 +0200

    environnement caché

--- environment.ts
+++ environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    baseApiUrl: 'https://compte-dons-9aazqqa5c-sulayman74s-projects.vercel.app/api/v1/'
+    baseApiUrl: process.env["API_URL"]
+    // baseApiUrl: 'https://compte-dons-sulayman74s-projects.vercel.app/api/v1/'
 };
